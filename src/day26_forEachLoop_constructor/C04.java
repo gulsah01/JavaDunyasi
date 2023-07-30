5b1ce9f307ac32c21bb0a44c19b4a2bb9698a126
gulsah01 <gulsahozkan90@gmail.com>
2023-07-30 21:15:59 +0100

    qwewqe

--- src/day26_forEachLoop_constructor/C04.java
+++ src/day26_forEachLoop_constructor/C04.java
@@ -1,11 +1,14 @@
 package day26_forEachLoop_constructor;
 
 public class C04 {
+    public static void main(String[] args) {
+
+    }
     int sayi =10;
     int kapasite;
     String ogrenciIsmi;
     int hiz;
     public void method1(){
-        System.out.println("method1 calisti");
+        System.out.println("method1 mi?");
     }
 }
