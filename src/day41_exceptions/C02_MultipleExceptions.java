72ef6c72b33aac36205731523041eb096e00817e
gulsah01 <gulsahozkan90@gmail.com>
2023-08-22 18:48:03 +0100

    update

---
+++ src/day41_exceptions/C02_MultipleExceptions.java
@@ -0,0 +1,13 @@
+package day41_exceptions;
+
+public class C02_MultipleExceptions {
+    public static void main(String[] args) {
+
+        String str="Java Candir";
+        int [] arr={3,5,7,8,9,0,};
+        //Kullanicidan bir sayi isteyip
+         // girilen sayiyi indev=x olarak kullanin
+        // str daki o index deki harfi
+        // ve array deki o indexdeki sayiyi
+    }
+}
