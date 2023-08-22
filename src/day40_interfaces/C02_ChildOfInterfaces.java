e4cb0bd933cbd78869cdc027c412f6476f28e29c
gulsah01 <gulsahozkan90@gmail.com>
2023-08-22 18:11:17 +0100

    update

--- src/day40_interfaces/C02_ChildOfInterfaces.java
+++ src/day40_interfaces/C02_ChildOfInterfaces.java
@@ -1,4 +1,25 @@
 package day40_interfaces;
 
-public class C02_ChildOfInterfaces {
+import day39_interfaces.I02_interfaces;
+
+public class C02_ChildOfInterfaces implements I01_interfaces, I02_interfaces{
+    @Override
+    public void method1() {
+    }
+    @Override
+    public int method2() {
+        return 0;
+    }
+    @Override
+    public int method11() {
+        return 0;
+    }
+    @Override
+    public String method3() {
+        return null;
+
+    }
+    @Override
+    public void method4() {
+    }
 }
